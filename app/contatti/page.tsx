3116220d03c281eafd0a66c1061d3b04b30a7530
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-05-02 10:04:22 +0200

    feat(pages): add new pages for blog, chi-sono, contatti
    - Add new pages for blog, chi-sono, and contatti
    - Rename Course page to CoursesPage

---
+++ app/contatti/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function ContattiPage() {
+	return <div>Contatti</div>
+}
--- app/corsi/page.tsx
+++ app/corsi/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
-export default function Course() {
+export default function CoursesPage() {
 	return <div>Course</div>
 }
