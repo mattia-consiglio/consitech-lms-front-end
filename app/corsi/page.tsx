c387c0327347c46e61bc01fb251c651d0d3cc149
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-05-09 19:47:00 +0200

    feat(courses): integrate courses API
    - Update CourseBlock component to accept nullable img prop
    - Fetch courses data from API in CoursesPage component
    - Display fetched courses using CourseBlock component
    - Add type definitions for API response data
    - Remove hardcoded course data
    - Delete unused page.ts file

--- app/corsi/page.tsx
+++ app/corsi/page.tsx
@@ -1,12 +1,87 @@
-'use client'
-
 import { setSubheaderTitle } from '@/redux/reducers/pageReducer'
 import { useAppDispatch } from '@/redux/store'
-import React, { useEffect } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import MainWrapper from '../components/MainWrapper'
 import CourseBlock from './CourseBlock'
 
-export default function CoursesPage() {
+export interface Couses {
+	content: Course[]
+	pageable: Pageable
+	last: boolean
+	totalPages: number
+	totalElements: number
+	size: number
+	number: number
+	sort: Sort
+	first: boolean
+	numberOfElements: number
+	empty: boolean
+}
+
+export interface Course {
+	id: string
+	mainLanguage: MainLanguage
+	translations: any[]
+	title: string
+	slug: string
+	description: string
+	publishStatus: string
+	createdAt: Date
+	displayOrder: number
+	thumbnail: null
+	seo: SEO
+	enrolledStudents: number
+}
+
+export interface MainLanguage {
+	id: string
+	code: string
+	language: string
+}
+
+export interface SEO {
+	id: string
+	mainLanguage: MainLanguage
+	translations: any[]
+	title: string
+	description: string
+	ldJSON: string
+}
+
+export interface Pageable {
+	pageNumber: number
+	pageSize: number
+	sort: Sort
+	offset: number
+	unpaged: boolean
+	paged: boolean
+}
+
+export interface Sort {
+	empty: boolean
+	unsorted: boolean
+	sorted: boolean
+}
+
+async function getData() {
+	const res = await fetch('http://localhost:3001/api/v1/public/courses')
+	// The return value is *not* serialized
+	// You can return Date, Map, Set, etc.
+
+	if (!res.ok) {
+		// This will activate the closest `error.js` Error Boundary
+		throw new Error('Failed to fetch data')
+	}
+
+	return res.json()
+}
+export default async function CoursesPage() {
+	// const [data, setData] = useState<Couses | undefined>(undefined)
+	// const [courses, setCourses] = useState<Course[]>([])
+
+	const data: Couses = await getData()
+	const courses = data?.content
+
 	return (
 		<MainWrapper subheaderTitle='Corsi'>
 			<div className='flex justify-center'>
@@ -22,87 +97,20 @@ export default function CoursesPage() {
 				</div>
 			</div>
 			<div className='grid grid-cols-1 md:grid-cols-3 gap-x-2 gap-y-4 mt-4'>
-				<CourseBlock
-					title='HTML'
-					description="Aggiungi stile alle tue pagine con colori, font, layout. Impara ad arricchire la struttura dell'HTML"
-					img={{
-						src: 'https://res.cloudinary.com/dqayns3d7/image/upload/v1714646312/css-logo.png',
-						alt: 'CSS logo',
-					}}
-					slug='html'
-				/>
-				<CourseBlock
-					title='CSS'
-					description="Aggiungi stile alle tue pagine con colori, font, layout. Impara ad arricchire la struttura dell'HTML"
-					img={{
-						src: 'https://res.cloudinary.com/dqayns3d7/image/upload/v1714646312/css-logo.png',
-						alt: 'CSS logo',
-					}}
-					slug='css'
-				/>
-				<CourseBlock
-					title='CSS'
-					description="Aggiungi stile alle tue pagine con colori, font, layout. Impara ad arricchire la struttura dell'HTML"
-					img={{
-						src: 'https://res.cloudinary.com/dqayns3d7/image/upload/v1714646312/css-logo.png',
-						alt: 'CSS logo',
-					}}
-					slug='#'
-				/>
-				<CourseBlock
-					title='CSS'
-					description="Aggiungi stile alle tue pagine con colori, font, layout. Impara ad arricchire la struttura dell'HTML"
-					img={{
-						src: 'https://res.cloudinary.com/dqayns3d7/image/upload/v1714646312/css-logo.png',
-						alt: 'CSS logo',
-					}}
-					slug='#'
-				/>
-				<CourseBlock
-					title='CSS'
-					description="Aggiungi stile alle tue pagine con colori, font, layout. Impara ad arricchire la struttura dell'HTML"
-					img={{
-						src: 'https://res.cloudinary.com/dqayns3d7/image/upload/v1714646312/css-logo.png',
-						alt: 'CSS logo',
-					}}
-					slug='#'
-				/>
-				<CourseBlock
-					title='CSS'
-					description="Aggiungi stile alle tue pagine con colori, font, layout. Impara ad arricchire la struttura dell'HTML"
-					img={{
-						src: 'https://res.cloudinary.com/dqayns3d7/image/upload/v1714646312/css-logo.png',
-						alt: 'CSS logo',
-					}}
-					slug='#'
-				/>
-				<CourseBlock
-					title='CSS'
-					description="Aggiungi stile alle tue pagine con colori, font, layout. Impara ad arricchire la struttura dell'HTML"
-					img={{
-						src: 'https://res.cloudinary.com/dqayns3d7/image/upload/v1714646312/css-logo.png',
-						alt: 'CSS logo',
-					}}
-					slug='#'
-				/>
-				<CourseBlock
-					title='CSS'
-					description="Aggiungi stile alle tue pagine con colori, font, layout. Impara ad arricchire la struttura dell'HTML"
-					img={{
-						src: 'https://res.cloudinary.com/dqayns3d7/image/upload/v1714646312/css-logo.png',
-						alt: 'CSS logo',
-					}}
-					slug='#'
-				/>
-				<CourseBlock
-					title='CSS'
-					description="Aggiungi stile alle tue pagine con colori, font, layout. Impara ad arricchire la struttura dell'HTML"
-					img={{
-						src: 'https://res.cloudinary.com/dqayns3d7/image/upload/v1714646312/css-logo.png',
-						alt: 'CSS logo',
-					}}
-					slug='#'
-				/>
+				<Suspense fallback={'Caricamento'}>
+					{courses.map(course => (
+						<CourseBlock
+							key={course.id}
+							title={course.title}
+							description={course.description}
+							img={{
+								src: 'https://res.cloudinary.com/dqayns3d7/image/upload/v1714646312/css-logo.png',
+								alt: 'CSS logo',
+							}}
+							slug={course.slug}
+						/>
+					))}
+				</Suspense>
 			</div>
 		</MainWrapper>
 	)
